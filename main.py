b5ff13adc001d5b87b5cc5baad189beb16e3b7e1
DnA3005 <danielabramov32@gmail.com>
2024-04-04 12:21:10 +0300

    added start and end times for tracking

--- main.py
+++ main.py
@@ -1,52 +1,8 @@
-# import cv2
-# import imutils
-  
-# # Initializing the HOG person
-# # detector
-# hog = cv2.HOGDescriptor()
-# hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-  
-# cap = cv2.VideoCapture('video_short.mp4')
-  
-# while cap.isOpened():
-#     # Reading the video stream
-#     ret, image = cap.read()
-#     if ret:
-#         image = imutils.resize(image, 
-#                                width=min(400, image.shape[1]))
-  
-#         # Detecting all the regions 
-#         # in the Image that has a 
-#         # pedestrians inside it
-#         (regions, _) = hog.detectMultiScale(image,
-#                                             winStride=(4, 4),
-#                                             padding=(4, 4),
-#                                             scale=1.05)
-  
-#         # Drawing the regions in the 
-#         # Image
-#         for (x, y, w, h) in regions:
-#             cv2.rectangle(image, (x, y),
-#                           (x + w, y + h), 
-#                           (0, 0, 255), 2)
-  
-#         # Showing the output Image
-#         cv2.imshow("Image", image)
-#         if cv2.waitKey(30) & 0xFF == ord('q'):
-#             break
-#     else:
-#         break
- 
-# cap.release()
-# cv2.destroyAllWindows()
-
-#-------------------------works---------------------------------
-
 import cv2
 import imutils
 
 # frame per second value
-FPS  = 30
+FPS = 30
 
 # Predefined colors for visualization
 COLORS = [(255, 0, 0), (130, 48, 219)]  # Blue, Pink
@@ -55,16 +11,45 @@ COLORS = [(255, 0, 0), (130, 48, 219)]  # Blue, Pink
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-cap = cv2.VideoCapture('video_short.mp4')
+cap = cv2.VideoCapture('video.mp4')
 
-person_colors = {}  # Dictionary to map person ID to color index
+# Define start and end time in seconds
+start_time = 3 * 60 + 18  # Convert 3 minutes and 18 seconds to seconds
+end_time = 4 * 60 + 33  # Convert 4 minutes and 33 seconds to seconds
+
+# Calculate the frame rate of the video
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+# Set the start and end frames based on the time range
+start_frame = int(start_time * fps)
+end_frame = int(end_time * fps)
+
+# Flag to indicate if tracking has started
+tracking_started = False
+
+# Initialize frame counter
+frame_count = 0
+
+# Dictionary to map person ID to color index
+person_colors = {}  
 
 while cap.isOpened():
     ret, image = cap.read()
-    if ret:
-        image = imutils.resize(image, width=min(400, image.shape[1]))
+    if not ret :
+        break
+    
+    image = imutils.resize(image, width=min(400, image.shape[1]))
 
-        # Detecting all the regions in the image with pedestrians inside it
+    # Start tracking when the start frame is reached
+    if frame_count >= start_frame:
+        tracking_started = True
+
+    # End tracking when the end frame is reached
+    if(frame_count >= end_frame):
+        tracking_started = False
+
+    if tracking_started:
+        # Detecting all the regions in the image with pedestrians inside itq
         (regions, _) = hog.detectMultiScale(image,
                                             winStride=(4, 4),
                                             padding=(4, 4),
@@ -82,13 +67,14 @@ while cap.isOpened():
                           (x + w, y + h),
                           color, 2)
 
-        cv2.imshow("Image", image)
+    cv2.imshow("Image", image)
 
-        # if 'q' is pressed exit all
-        if cv2.waitKey(FPS) & 0xFF == ord('q'):
-            break
-    else:
+    # If 'q' is pressed exit
+    if cv2.waitKey(FPS) & 0xFF == ord('q'):
         break
+
+    frame_count += 1
+
 
 cap.release()
 cv2.destroyAllWindows()
